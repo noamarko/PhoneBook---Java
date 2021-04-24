d20f61a0b268d941c2bd7d110cd542c5bd1f7d74
noamarko <59430912+noamarko@users.noreply.github.com>
2021-04-24 10:29:51 +0300

    Delete Restaurant_Creator directory

--- Restaurant_Creator/src/restaurant_creator/ProductFunction.java
+++
@@ -1,9 +0,0 @@
-package restaurant_creator;
-
-public interface ProductFunction extends Comparable<Product>, Cloneable {
-
-	int compareTo(Product prdct);
-	
-	Object clone(Product pdt) throws PriceException;
-		
-}
